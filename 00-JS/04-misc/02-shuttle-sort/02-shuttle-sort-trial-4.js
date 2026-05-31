821540d75b40105e0528b195a596fca7524ee893
nirajsbajaj <bajajsniraj@gmail.com>
2026-06-01 01:10:39 +0530

    derive bidirectional shuttle sort variant from bubble sort

---
+++ 00-JS/04-misc/02-shuttle-sort/02-shuttle-sort-trial-4.js
@@ -0,0 +1,28 @@
+function shuttleSort(inputArr) {
+    let left = 0;
+    let right = inputArr.length - 1;
+
+    let minValueAt = 0;
+
+    for(let outside = right; outside > left; outside--) {
+
+        for(let inside = left+1; inside <= outside; inside++) {
+            
+            if(inputArr[inside-1] > inputArr[inside]) {
+                [inputArr[inside-1] , inputArr[inside]] = [inputArr[inside] , inputArr[inside-1]] ;
+                
+                if(inputArr[minValueAt] > inputArr[inside-1]) {
+                    [inputArr[minValueAt] , inputArr[inside-1]] = [inputArr[inside-1] , inputArr[minValueAt]] ;
+                }
+
+            }
+
+        }
+
+        minValueAt++;
+    }
+
+    return inputArr;
+}
+
+console.log(shuttleSort([34, 12, 22, 9, 4, 60, 56, 14]));
